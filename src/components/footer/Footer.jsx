6d9bc86ddac544b10440e6d034ae7554cd6cb784
Marco Franca <dev.marcotullio@gmail.com>
2022-09-20 08:27:05 -0300

    footer file criate [ADD]

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>blablabla</li>
+                <li>blablabla</li>
+                <li>blablabla</li>
+            </ul>
+        </footer>
+    )
+}
