52d2b4e297a6b25d33d7bc21c87cc8a906ea946e
Marco Franca <dev.marcotullio@gmail.com>
2022-09-20 08:34:50 -0300

    inclusion of details page [APDATE]

--- src/assets/router/Router.jsx
+++ src/assets/router/Router.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../../pages/home/Home";
 import PageNotFound from "../../pages/error/PageNotFound";
+import Details from "../../pages/detail/Details";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<Home/>}/>
+                <Route path="./details" element={<Details/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
