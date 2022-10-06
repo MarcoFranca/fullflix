e45226c92a97d83862600967920e6861813b88e5
Marco Franca <dev.marcotullio@gmail.com>
2022-10-06 18:58:27 -0300

    inclusion of link to detail page and function to capture card information [UPDATE]

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,16 +1,29 @@
 import React, {useEffect, useState} from "react";
-import {getList} from "../../assets/api/req";
+import {getDetails, getList} from "../../services/apis/req";
 import Loading from "../../assets/images/loading.gif"
 import {Main, MainSectionContainer} from "./CardsStyle";
+import {useDispatch} from "react-redux";
+import {getMovieDetails, goToDetail} from "../../assets/redux/counterSlice";
+import {Link} from "react-router-dom";
 
 export default function Cards() {
     const [movies, setMovies] = useState([])
+    const [details, setDetails] = useState()
+    const dispatch = useDispatch();
 
     useEffect( ()=>{
         getList(setMovies)
     },[])
 
+    useEffect(()=>{
+        dispatch(getMovieDetails(details))
+    },[details,dispatch])
+
     console.log(movies)
+
+    const onHouverDetail = (event)=>{
+        getDetails(event.target.id, setDetails)
+    }
 
     return (
         <Main>
@@ -18,11 +31,17 @@ export default function Cards() {
             {!movies ? (<img src={Loading} alt={"Loading..."}/>): (
                 <MainSectionContainer>
                     {movies.map((movie)=>(
-                            <div className="card" key={movie.id}>
-                                <img key={movie.id} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} />
-                                <h2>Título: {movie.title}</h2>
-                                <p>Nota: {movie.vote_average}</p>
-                            </div>
+                            <Link to="/details" key={movie.id}>
+                                <div className="card" key={movie.id}
+                                     onClick={()=> {dispatch(goToDetail())}}
+                                     onMouseEnter={ onHouverDetail}
+                                     id={movie.id}
+                                >
+                                    <img id={movie.id} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} />
+                                    <h2>Título: {movie.title}</h2>
+                                    <p>Nota: {movie.vote_average}</p>
+                                </div>
+                            </Link>
                         )
                     )}
                 </MainSectionContainer>
