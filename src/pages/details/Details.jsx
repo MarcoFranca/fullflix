2d6f7079f4f9966dbc7af87ee4a91af7dfa5a076
Marco Franca <dev.marcotullio@gmail.com>
2022-10-06 18:54:54 -0300

    details and styling page [UPDATE]

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import CardDetail from "../../components/cardDetail/CardDetail";
+import {useSelector} from "react-redux";
+import Footer from "../../components/footer/Footer";
+import {Header} from "./DetailsStyle";
+import Loading from "../../assets/images/Spinner.svg"
+
+
+
 
 export default function Details() {
+    const state = useSelector((state)=> state.page.value2)
+
+            console.log(state)
     return (
-        <main>
-            <h1>Details</h1>
-        </main>
+        (state === undefined) ?
+            <div>
+                <img src={Loading} alt="loading" style={{width:"50px"}}/>
+            </div>
+            :
+        <>
+            <Header>
+                <h1>{state.data.title}</h1>
+            </Header>
+
+            <CardDetail/>
+            <Footer/>
+        </>
+
+
     )
 }
