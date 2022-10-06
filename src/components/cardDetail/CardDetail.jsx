d85be6182f710c8f381adbba71c7f6150bc77138
Marco Franca <dev.marcotullio@gmail.com>
2022-10-06 18:57:24 -0300

    information has been added to the card detail [UPDATE]

--- src/components/cardDetail/CardDetail.jsx
+++ src/components/cardDetail/CardDetail.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {DivCardContent, DivContainer, LinkButton} from "./CardDetailStyle";
+import {format} from "date-fns";
 
-export default function CardDetail(props) {
+export default function CardDetail() {
+    const state = useSelector((state)=> state.page.value2)
+
     return (
-        <div>
-            <img src={props.urlImg} alt={props.title}/>
-            <div className="content">
-                <h1>{props.title}</h1>
-                <p>{props.details}</p>
-            </div>
-        </div>
+            <DivContainer key={state.id} className="card">
+                <img src={`https://image.tmdb.org/t/p/w500${state.data.poster_path}`} alt={state.data.title}/>
+                <DivCardContent className="card__content">
+                    <h2>Título: <span>{state.data.title}</span></h2>
+                    <h2>Sinopse: <span>{state.data.overview}</span></h2>
+                    <h2>Lançamento: <span>{format(new Date(state.data.release_date), 'dd/MM/yyyy')}</span></h2>
+                    <h2>Nota: <span>{state.data.vote_average.toFixed(2)}</span></h2>
+                    <LinkButton to="/" key={state.data.id} >
+                         Voltar para Home
+                    </LinkButton>
+                </DivCardContent>
+            </DivContainer>
     )}
