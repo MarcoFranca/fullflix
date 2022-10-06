311eae27bb4db863566d8426b8c6987ea5538da4
Marco Franca <dev.marcotullio@gmail.com>
2022-10-06 18:50:52 -0300

    include api settings [UPDATE]

--- src/services/apis/apiConfig.js
+++ src/services/apis/apiConfig.js
@@ -1,3 +1,9 @@
 export const key = "api_key=79810a14541b1782eda2bb3c4f2c7d7e"
 export const url = "https://api.themoviedb.org/3"
 export const languageBr = "language=pt-BR"
+
+export const api = {
+    key: "api_key=79810a14541b1782eda2bb3c4f2c7d7e",
+    url: "https://api.themoviedb.org/3",
+    languageBr: "language=pt-BR"
+}
