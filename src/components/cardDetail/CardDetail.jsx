c7f965223c72b6c1219e676c52856aa925b981fb
Marco Franca <dev.marcotullio@gmail.com>
2022-10-06 23:30:45 -0300

    tag change[UPDATE]

--- src/components/cardDetail/CardDetail.jsx
+++ src/components/cardDetail/CardDetail.jsx
@@ -8,12 +8,14 @@ export default function CardDetail() {
 
     return (
             <DivContainer key={state.id} className="card">
+                <div className="img">
                 <img src={`https://image.tmdb.org/t/p/w500${state.data.poster_path}`} alt={state.data.title}/>
+                </div>
                 <DivCardContent className="card__content">
-                    <h2>Título: <span>{state.data.title}</span></h2>
-                    <h2>Sinopse: <span>{state.data.overview}</span></h2>
-                    <h2>Lançamento: <span>{format(new Date(state.data.release_date), 'dd/MM/yyyy')}</span></h2>
-                    <h2>Nota: <span>{state.data.vote_average.toFixed(2)}</span></h2>
+                    <h2><b>Título: </b>{state.data.title}</h2>
+                    <h2><b>Sinopse: </b> {state.data.overview}</h2>
+                    <h2><b>Lançamento:</b>{format(new Date(state.data.release_date), 'dd/MM/yyyy')}</h2>
+                    <h2><b>Nota: </b>{state.data.vote_average.toFixed(2)}</h2>
                     <LinkButton to="/" key={state.data.id} >
                          Voltar para Home
                     </LinkButton>
