1f973608535344fea446b9c398d7d21085bc683c
Marco Franca <dev.marcotullio@gmail.com>
2022-10-07 18:42:38 -0300

    add redux state[UPDATE]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {FooterStyle} from "./FooterStyle";
+import {useSelector} from "react-redux";
 
 export default function Footer() {
+    const state2 = useSelector((state)=> state.page.value)
     return (
         <FooterStyle>
-            <h3>React movies © 2022 todos os direitos reservados. </h3>
+            <h3> React movies © 2022 todos os direitos reservados. | {state2} </h3>
         </FooterStyle>
     )
 }
