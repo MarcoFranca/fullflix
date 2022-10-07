eb71f6445185ec2af4fef3370061b75849e2908c
Marco Franca <dev.marcotullio@gmail.com>
2022-10-06 23:33:33 -0300

    function to switch pages[UPDATE]

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -4,7 +4,7 @@ import Loading from "../../assets/images/loading.gif"
 import {Main, MainSectionContainer} from "./CardsStyle";
 import {useDispatch} from "react-redux";
 import {getMovieDetails, goToDetail} from "../../assets/redux/counterSlice";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export default function Cards() {
     const [movies, setMovies] = useState([])
@@ -17,12 +17,21 @@ export default function Cards() {
 
     useEffect(()=>{
         dispatch(getMovieDetails(details))
-    },[details,dispatch])
+    },[details])
 
     console.log(movies)
 
     const onHouverDetail = (event)=>{
         getDetails(event.target.id, setDetails)
+        console.log(details)
+    }
+    let navigate = useNavigate()
+
+    const click = (event, navigate) => {
+        dispatch(goToDetail())
+        onHouverDetail(event)
+        setTimeout(() =>
+        {navigate("/details")}, 150)
     }
 
     return (
@@ -31,17 +40,17 @@ export default function Cards() {
             {!movies ? (<img src={Loading} alt={"Loading..."}/>): (
                 <MainSectionContainer>
                     {movies.map((movie)=>(
-                            <Link to="/details" key={movie.id}>
+                            <div key={movie.id}>
                                 <div className="card" key={movie.id}
-                                     onClick={()=> {dispatch(goToDetail())}}
-                                     onMouseEnter={ onHouverDetail}
+                                     onClick={(event)=> {
+                                         click(event,navigate)}}
                                      id={movie.id}
                                 >
                                     <img id={movie.id} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} />
                                     <h2>Título: {movie.title}</h2>
                                     <p>Nota: {movie.vote_average}</p>
                                 </div>
-                            </Link>
+                            </div>
                         )
                     )}
                 </MainSectionContainer>
