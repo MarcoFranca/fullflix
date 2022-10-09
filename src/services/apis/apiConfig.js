f43ea9fdf3ebfabc3421501a9a45c2e8fadc61f3
Marco Franca <dev.marcotullio@gmail.com>
2022-10-09 10:36:31 -0300

    new API settings [UPDATE]

--- src/services/apis/apiConfig.js
+++ src/services/apis/apiConfig.js
@@ -1,9 +1,7 @@
-export const key = "api_key=79810a14541b1782eda2bb3c4f2c7d7e"
-export const url = "https://api.themoviedb.org/3"
-export const languageBr = "language=pt-BR"
-
 export const api = {
     key: "api_key=79810a14541b1782eda2bb3c4f2c7d7e",
-    url: "https://api.themoviedb.org/3",
-    languageBr: "language=pt-BR"
+    token:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTgxMGExNDU0MWIxNzgyZWRhMmJiM2M0ZjJjN2Q3ZSIsInN1YiI6IjYzMjViYmVkN2Q0MWFhMDA3ZGQwNjRjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pO1Hby-ABrhWKJVlPIIwDQyFSgv9G7aYC9kvtt6UyFA',
+    urlBase: "https://api.themoviedb.org/3/movie/",
+    languageBr: "&language=pt-BR",
+    popular: "popular?"
 }
