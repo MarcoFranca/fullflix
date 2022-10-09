f24d87954dc6f1d542ef4e38f95ba92661acccf8
Marco Franca <dev.marcotullio@gmail.com>
2022-10-09 11:40:10 -0300

    added home/detail in title [UPDATE]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,7 @@ import {useSelector} from "react-redux";
 
 export default function Footer() {
     const state2 = useSelector((state)=> state.page.value)
+    document.title = `${state2} | Fullflix`
     return (
         <FooterStyle>
             <h3> React movies © 2022 todos os direitos reservados. | {state2} </h3>
